a329e76430c1b0ce951e0ac1fce9c94e2465e988
pratibha-negi01 <111881226+pratibha-negi01@users.noreply.github.com>
2022-11-26 16:07:51 +0530

    Delete go2uvm_example directory

--- go2uvm_example/Fifo_go2uvm_verification_env/ENV/fifo_sequencer.sv
+++
@@ -1,9 +0,0 @@
-class fifo_sequencer extends uvm_sequencer#(fifo_trans);
-  
-  `uvm_component_utils(fifo_sequencer)
-  
-  function new(string name="", uvm_component parent);
-    super.new(name, parent);
-  endfunction
-  
-endclass
