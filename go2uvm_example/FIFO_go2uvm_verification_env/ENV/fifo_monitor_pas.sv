a31b3e1a4f0bfd7db2cc8cadb7aab85a8f762bcf
pratibha-negi01 <111881226+pratibha-negi01@users.noreply.github.com>
2022-11-28 14:41:21 +0530

    Update fifo_monitor_pas.sv

--- go2uvm_example/FIFO_go2uvm_verification_env/ENV/fifo_monitor_pas.sv
+++ go2uvm_example/FIFO_go2uvm_verification_env/ENV/fifo_monitor_pas.sv
@@ -13,10 +13,12 @@ class fifo_monitor_pas extends uvm_monitor;
   
   task run_phase(uvm_phase phase);
     super.run_phase(phase);
-    @(posedge vif.clk);
+    @(negedge vif.clk);
     repeat(`number)
       begin
-        @(posedge vif.clk);
+        @(negedge vif.push or negedge vif.pop);
+       // @(negedge vif.clk);
+        //@(posedge vif.push or posedge vif.pop);
       tns=new("tns");
         //@(posedge vif.sysclk);
         tns.full=vif.full;
@@ -24,15 +26,16 @@ class fifo_monitor_pas extends uvm_monitor;
         tns.empty=vif.empty;
         `uvm_info(get_type_name(),$sformatf("input value monitor  EMPTY=%d",vif.empty),UVM_LOW);
         tns.push_err_on_full=vif.push_err_on_full;
-        `uvm_info(get_type_name(),$sformatf("input value monitor  PUSH_ERR_ON_FULL=%d",vif.push_err_on_full),UVM_LOW);
+       `uvm_info(get_type_name(),$sformatf("input value monitor  PUSH_ERR_ON_FULL=%d",vif.push_err_on_full),UVM_LOW);
         tns.pop_err_on_empty=vif.pop_err_on_empty;
-        `uvm_info(get_type_name(),$sformatf("input value monitor  PUSH_ERR_ON_EMPTY=%d",vif.pop_err_on_empty),UVM_LOW);
+       `uvm_info(get_type_name(),$sformatf("input value monitor  PUSH_ERR_ON_EMPTY=%d",vif.pop_err_on_empty),UVM_LOW);
+        //@(posedge vif.push or vif.pop);
       tns.data_out=vif.data_out;
         `uvm_info(get_type_name(),$sformatf("input value monitor  DATA_OUT=%d",vif.data_out),UVM_LOW);
   
       
       mon_aport.write(tns);
-        @(negedge vif.clk);
+      //  @(negedge vif.clk);
       //`uvm_info(get_type_name(),"monitor",UVM_LOW);
     end
   endtask
