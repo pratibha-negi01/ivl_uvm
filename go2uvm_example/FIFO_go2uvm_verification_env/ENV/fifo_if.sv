d2b07ee9c45412c4b5d26e60d8574e0c271001d0
pratibha-negi01 <111881226+pratibha-negi01@users.noreply.github.com>
2022-11-29 11:51:53 +0530

    Update fifo_if.sv

--- go2uvm_example/FIFO_go2uvm_verification_env/ENV/fifo_if.sv
+++ go2uvm_example/FIFO_go2uvm_verification_env/ENV/fifo_if.sv
@@ -1,9 +1,29 @@
-`define DATA_WIDTH 8
-`define ADDR_WIDTH 4
-interface fifo_if(input clk);
+import fifo_pkg::*;
+
+interface fifo_if(input logic clk);
+  logic rst,push,pop,full,empty;
   logic [`DATA_WIDTH-1:0] data_in;
-  logic rst_n,push,pop;
   logic [`DATA_WIDTH-1:0] data_out;
-  logic push_err_on_full,pop_err_on_empty, full,empty;
-endinterface
+  logic push_err_on_full,pop_err_on_empty;
   
+  clocking driver_cb@(posedge clk);
+    
+    output rst,push,pop;
+    output data_in;
+    input data_out;
+    input full,empty;
+    input push_err_on_full,pop_err_on_empty;
+    
+  endclocking : driver_cb
+  
+  clocking monitor_cb@(posedge clk);
+    input rst,push,pop;
+    input data_in;
+    input data_out;
+    input full,empty;
+    input push_err_on_full,pop_err_on_empty;
+    
+  endclocking : monitor_cb
+ 
+  
+endinterface : fifo_if
